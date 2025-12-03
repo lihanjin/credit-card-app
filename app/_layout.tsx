7b2f479814f59fd2cff896c5ead42a9189c1793c
leo.li <leo.li@chixi88.com>
2025-12-03 17:07:30 +0800

    refactor: 更新项目配置和组件结构
    - 移除 babel.config.js 中的注释
    - 更新 VSCode 设置，添加自动格式化功能
    - 在 _layout.tsx 中引入全局样式
    - 优化 index.tsx 组件结构，使用 Tailwind CSS 类名

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import "../assets/css/global.css";
 
 export default function RootLayout() {
   return <Stack />;
--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,6 @@ module.exports = function (api) {
     ],
     plugins: [
       // react-native-reanimated 插件必须在所有插件之后
-      // 注意：expo-router 已经包含在 babel-preset-expo 中，不需要单独添加
       require.resolve("react-native-reanimated/plugin"),
     ],
   };
